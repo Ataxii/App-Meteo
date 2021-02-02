723cd7530a95cbf48fd3b0ef519d789812d8319c
Theo <theo.martinet@etu.univ-amu.fr>
2021-02-02 12:16:33 +0100

    new command in favourite

--- src/main/java/app/appmeteo/controller/Favourite.java
+++ src/main/java/app/appmeteo/controller/Favourite.java
@@ -1,41 +1,59 @@
 package app.appmeteo.controller;
 
 import java.io.*;
-import java.nio.file.Paths;
 
 public class Favourite {
 
     public static void addFavourite(String favourite) throws IOException {
-        String data = reader();
-        data += favourite;
-        String[] dataString = data.split("\n");
-        writter(dataString);
+        if (isFavourite(favourite)){
+            System.out.println("Is already in your favourite");
+        }else {
+            String data = reader();
+            data += favourite;
+            String[] dataString = data.split("\n");
+            writter(dataString);
+        }
     }
 
     public static void deleteFavourite(String favourite) throws IOException {
-        String data = reader();
-        String[] dataString = data.split("\n");
-        for (int i = 0; i < dataString.length; i++) {
-            if (dataString[i].equals(favourite)){
-                dataString[i] = "";
-            }
+        if (!isFavourite(favourite)){
+            System.out.println("Is not already in your favourite");
         }
-        writter(dataString);
+        else {
+            String data = reader();
+            String[] dataString = data.split("\n");
+            for (int i = 0; i < dataString.length; i++) {
+                if (dataString[i].equals(favourite)){
+                    dataString[i] = "";
+                }
+            }
+            writter(dataString);
+        }
     }
 
+    public static boolean isFavourite (String fav) throws IOException {
+        String data = reader();
+        String[] dataString = data.split("\n");
+        for (String s : dataString) {
+            if (s.equals(fav)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    public static void writter (String[] data) throws IOException {
+    private static void writter (String[] data) throws IOException {
         //write the new favourite with data
         BufferedWriter w = new BufferedWriter(new FileWriter("Favourite.txt"));
-        for (int i = 0; i < data.length; i++) {
-            if (!data[i].isEmpty()){
-                w.append(data[i]);
+        for (String datum : data) {
+            if (!datum.isEmpty()) {
+                w.append(datum);
                 w.newLine();
             }
         }
     }
 
-    public static String reader () throws IOException {
+    private static String reader() throws IOException {
         //recuperation of data in new reload program
         BufferedReader r = new BufferedReader(new FileReader("favourite.txt"));
         String lineRead;
@@ -44,5 +62,9 @@ public class Favourite {
             data = data + lineRead + "\n";
         }
         return data;
+    }
+
+    public static String[] FavouriteList () throws IOException {
+        return reader().split("\n");
     }
 }
