ecf4de9419ba5c6108451a826a9c0534bc10648d
CLOP Cody <cody.clop@etu.univ-amu.fr>
2021-02-05 18:54:46 +0100

    APIQuery with ID, Country Code and ZIP Code

--- src/main/java/app/appmeteo/controller/APIQuery.java
+++ src/main/java/app/appmeteo/controller/APIQuery.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 /**
  * Controller to query API
- * @version 1.1
+ * @version 1.2
  */
 public class APIQuery {
     private static final String key = "5fbdee4725692638183914f166ab5066"; // API Key
@@ -61,6 +61,78 @@ public class APIQuery {
         return r.readLine();
     }
 
+    /**
+     * Creates a query for the API asking for current weather in the city passed in parameter
+     * @param city the String corresponding to city's name
+     * @param countryCode the String corresponding to city's country code
+     * @throws IOException if the URL sent is not valid
+     * @return a string containing all .json infos from the API answer
+     * @since 1.2
+     */
+    public static String QueryWithCountryCode(String city, String countryCode) throws IOException {
+        // API Query
+        String StrUrl = "http://api.openweathermap.org/data/2.5/weather?q="+ city + "," + countryCode +
+                "&units=metric&appid=" + key;
+
+        URL url = new URL(StrUrl);
+        // URL connection
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        // Buffer creation
+        InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+        BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+
+        // return string
+        return r.readLine();
+    }
+
+    /**
+     * Creates a query for the API asking for current weather in the city passed in parameter
+     * @param id the String corresponding to city's ID
+     * @throws IOException if the URL sent is not valid
+     * @return a string containing all .json infos from the API answer
+     * @since 1.2
+     */
+    public static String QueryWithID(String id) throws IOException {
+        // API Query
+        String StrUrl = "http://api.openweathermap.org/data/2.5/weather?id="+ id +"&units=metric&appid=" + key;
+
+        URL url = new URL(StrUrl);
+        // URL connection
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        // Buffer creation
+        InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+        BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+
+        // return string
+        return r.readLine();
+    }
+
+    /**
+     * Creates a query for the API asking for current weather in the city passed in parameter
+     * @param zip the String corresponding to city's ZIP Code
+     * @param countryCode the String corresponding to city's country code
+     * @throws IOException if the URL sent is not valid
+     * @return a string containing all .json infos from the API answer
+     * @since 1.2
+     */
+    public static String QueryWithZip(String zip, String countryCode) throws IOException {
+        // API Query
+        String StrUrl = "http://api.openweathermap.org/data/2.5/weather?zip="+ zip + "," + countryCode +"&units=metric&appid=" + key;
+
+        URL url = new URL(StrUrl);
+        // URL connection
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        // Buffer creation
+        InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+        BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+
+        // return string
+        return r.readLine();
+    }
+
 
     /**
      * Creates a query for the API asking for weather in coordinates passed in parameter
@@ -69,6 +141,7 @@ public class APIQuery {
      * @param latitude latitude of the city
      * @return a string containing all .json infos from the API answer
      * @throws IOException if the URL sent is not valid
+     * @since 1.1
      */
     public static String QueryWithPos(double longitude, double latitude) throws IOException {
         // API Query
---
+++ src/test/java/app/appmeteo/controller/APIQueryTest.java
@@ -0,0 +1,40 @@
+package app.appmeteo.controller;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class APIQueryTest {
+    @Test
+    public void queryTest() throws IOException {
+        String response = APIQuery.QueryWithCountryCode("Paris", "US");
+        JsonObject obj = JsonParser.parseString(response).getAsJsonObject();
+        int code = obj.get("cod").getAsInt();
+        assertEquals(code, 200);
+
+        response = APIQuery.QueryWithID("2172797");
+        obj = JsonParser.parseString(response).getAsJsonObject();
+        code = obj.get("cod").getAsInt();
+        assertEquals(code, 200);
+
+        response = APIQuery.QueryWithPos(3.1195, 43.238);
+        System.out.println(response);
+        obj = JsonParser.parseString(response).getAsJsonObject();
+        double lat = obj.get("lat").getAsDouble();
+        assertEquals(lat, 43.238);
+
+        response = APIQuery.QueryWithZip("13005", "FR");
+        obj = JsonParser.parseString(response).getAsJsonObject();
+        code = obj.get("cod").getAsInt();
+        assertEquals(code, 200);
+
+        response = APIQuery.QueryStringWithCity("London");
+        obj = JsonParser.parseString(response).getAsJsonObject();
+        code = obj.get("cod").getAsInt();
+        assertEquals(code, 200);
+    }
+}
