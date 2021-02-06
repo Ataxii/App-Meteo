a4bb1c5444a66e3076b818185f64f7341858d704
b18007151 <yoan.bouniard@etu.uni-amu.fr>
2021-02-06 17:33:22 +0100

    Ajout attribut favoris (déplacer depuis CLIController.java)

--- src/main/java/app/appmeteo/controller/User.java
+++ src/main/java/app/appmeteo/controller/User.java
@@ -1,32 +1,42 @@
 package app.appmeteo.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class UserQuery {
+public class User {
 
     private String[] query;
     private boolean hasDate;
     private final Scanner scanner;
+    public FavouriteList favouriteList;
 
-    public UserQuery(Scanner scanner) {
+    public User(Scanner scanner) throws IOException {
         this.scanner = scanner;
+        this.favouriteList = new FavouriteList();
     }
 
     /**
-     * for testing purpose
-     * @param query
+     * @param query the commands line
      */
-    public UserQuery(String[] query) {
+    public User(String[] query) throws IOException {
         this.query = query;
         this.scanner = null;
+        this.favouriteList = new FavouriteList();
     }
 
     public String[] getQuery() {
         return query;
     }
 
+    public String[] getQuery(int from) {
+        return Arrays.copyOfRange(query, from, query.length);
+    }
+
+    /**
+     * This method is used to query a new command from the user thanks to the scanner instance
+     */
     public void next(){
         String line = this.scanner.nextLine();
         this.query =  line.split(" ");
@@ -36,7 +46,7 @@ public class UserQuery {
     /**
      * the goal of this function is to compact the String array received through the scanner into a logical new array
      * with command in each array element. for example, this : [Los, Angeles, -temp, -wind] would become [Los Angeles, -temp, -wind]
-     * it is public for testing purpose
+     * it is currently public for testing purpose
      */
     public void fixCommandline() {
         // before options
@@ -51,16 +61,19 @@ public class UserQuery {
                 else newCommandLine.set(0, newCommandLine.get(0) + " " + s);
             }
         }
-        query = newCommandLine.toArray(query);
+        query = newCommandLine.toArray(new String[0]);
     }
 
     // this [Los Angeles 25/10/21, -temp, -wind] become this: [Los Angeles, 25/10/21, -temp, -wind]
     public void fixCommandDate() {
-
     }
 
     public String getCommandType() {
         return query[0];
+    }
+
+    public int getQueryLength() {
+        return query.length;
     }
 
     public boolean hasDate() {
